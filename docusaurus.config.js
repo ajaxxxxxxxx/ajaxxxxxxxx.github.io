db321cd0f74d5facf1171632fd2ca5d93064bc61
TheMrNewVegas <112358568+TheMrNewVegas@users.noreply.github.com>
2023-11-29 09:05:48 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://themrnewvegas.github.io',
+  url: 'https://modlists.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
