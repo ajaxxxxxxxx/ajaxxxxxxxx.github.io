681bd5369d33096c322a77288a45e723441bb107
themrnewvegas <themrnewvegas8@gmail.com>
2023-11-30 21:49:56 +0000

    deploy: c0e886cbacffc2ee5915e35f728c93a37b582c2f

--- assets/js/5dcb3309.de008b96.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[5829],{8890:e=>{e.exports=JSON.parse('{"label":"HOS","permalink":"/blog/tags/hos","allTagsPath":"/blog/tags","count":2,"unlisted":false}')}}]);
---
+++ assets/js/fc4555c2.0bf86da1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[424],{8424:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/capital-punishment","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
