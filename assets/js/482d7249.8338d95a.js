0348728f0c889ce6604aa50eeca430f5541d9649
themrnewvegas <themrnewvegas8@gmail.com>
2023-12-04 22:49:13 +0000

    deploy: b9d00d9a1fd54620d79cd51a3421f8737a5fd702

---
+++ assets/js/482d7249.8338d95a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[3102],{6488:e=>{e.exports=JSON.parse('{"label":"Capital Punishment","permalink":"/blog/tags/capital-punishment","allTagsPath":"/blog/tags","count":7,"unlisted":false}')}}]);
