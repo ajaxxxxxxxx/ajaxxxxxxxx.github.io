7e854593ba9da85f0fa20c425e087d3a9b42a82a
TheMrNewVegas <112358568+TheMrNewVegas@users.noreply.github.com>
2025-02-25 08:42:56 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://KitchenSink.net',
+  url: 'https://KitchenSink.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/KitchenSink.github.io',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
