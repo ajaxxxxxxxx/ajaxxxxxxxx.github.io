9b4f4bb305720fd8cae99a932ac012ed8dbdf0bd
TheMrNewVegas <112358568+TheMrNewVegas@users.noreply.github.com>
2025-03-04 14:02:07 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   projectName: 'ajaxxxxxxxx.github.io', // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'ignore',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
