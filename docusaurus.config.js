953b72abeca0de17c2563b3426eadefdd8a9130d
ajaxxxxxxxx <166178246+ajaxxxxxxxx@users.noreply.github.com>
2025-03-10 16:46:26 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,8 +24,8 @@ const config = {
   projectName: 'ajaxxxxxxxx.github.io', // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'ignore',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
